5190a881be1213003c29633258a30ff1563c1e11
furstepnik <furstepnik@yandex.ru>
2021-12-24 15:49:09 +0300

    Scripted auto-commit on change (2021-12-24 15:49:09) by gitwatch.sh

--- src/main/java/bmstu/TestMessage.java
+++ src/main/java/bmstu/TestMessage.java
@@ -1,7 +1,20 @@
 package bmstu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class TestMessage {
     private String packageId;
     private String nameFunction;
     private List<Test> tests;
+    private String script;
+
+    private final String PACKAGE_ID = "packageId";
+    private final String FUNCTION_NAME = "functionName";
+    private final String SCRIPT = "jsScript";
+    private final String TESTS = "tests";
+
+    @JsonCreator
+    public TestMessage(
+            
+    )
 }
