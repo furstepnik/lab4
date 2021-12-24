356e323bbcd674d25e71e236226d89e0b71b7fc1
furstepnik <furstepnik@yandex.ru>
2021-12-24 14:02:19 +0300

    Scripted auto-commit on change (2021-12-24 14:02:19) by gitwatch.sh

--- src/main/java/bmstu/AkkaApp.java
+++ src/main/java/bmstu/AkkaApp.java
@@ -1,5 +1,13 @@
 package bmstu;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 public class AkkaApp {
-    
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("AkkaApp");
+        ActorRef 
+    }
 }
