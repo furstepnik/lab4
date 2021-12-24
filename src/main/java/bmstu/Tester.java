fd52d960d93d68c2f684003065c5282482f12877
furstepnik <furstepnik@yandex.ru>
2021-12-24 16:13:22 +0300

    Scripted auto-commit on change (2021-12-24 16:13:22) by gitwatch.sh

--- src/main/java/bmstu/Tester.java
+++ src/main/java/bmstu/Tester.java
@@ -16,6 +16,7 @@ public class Tester extends AbstractActor {
 
     private String runTest(Test test) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS);
+        TestMessage parent = test.getTest();
         
     }
 }
