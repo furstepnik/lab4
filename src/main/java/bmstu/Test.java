3b46273a0421fe23d21b6183d2364bd896504c9a
furstepnik <furstepnik@yandex.ru>
2021-12-24 15:44:11 +0300

    Scripted auto-commit on change (2021-12-24 15:44:11) by gitwatch.sh

--- src/main/java/bmstu/Test.java
+++ src/main/java/bmstu/Test.java
@@ -1,5 +1,55 @@
 package bmstu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
-    private String 
+    private String testName;
+    private String expectedResult;
+    private String actualResult;
+    private TestMessage test;
+    private Object[] parameters;
+    private static final String NAME_TEST = "teatName";
+    private static final String EXPECTED_RESULT = "expectedResult";
+    private static final String PARAMETERS = "params";
+
+    @JsonCreator
+    public Test(
+            @JsonProperty(NAME_TEST) String nameTest,
+            @JsonProperty(EXPECTED_RESULT) String res,
+            @JsonProperty(PARAMETERS) Integer[] p
+    ) {
+        this.testName = nameTest;
+        this.expectedResult = res;
+        this.parameters = p;
+        this.actualResult = "";
+    }
+
+    public String getTestName() {
+        return this.testName;
+    }
+
+    public String getExpectedResult() {
+        return this.expectedResult;
+    }
+
+    public Object[] getParameters () {
+        return this.parameters;
+    }
+
+    public TestMessage getTest() {
+        return this.test;
+    }
+
+    public String getActualResult() {
+        return this.actualResult;
+    }
+
+    public void setTest(TestMessage p) {
+        this.test = p;
+    }
+
+    public void setActualResult(String r) {
+        this.actualResult = r;
+    }
 }
