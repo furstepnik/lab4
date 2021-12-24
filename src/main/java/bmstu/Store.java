ba8a59393a61a3af6d38f4cc4aa88c7615cf48fe
furstepnik <furstepnik@yandex.ru>
2021-12-24 16:01:46 +0300

    Scripted auto-commit on change (2021-12-24 16:01:46) by gitwatch.sh

--- src/main/java/bmstu/Store.java
+++ src/main/java/bmstu/Store.java
@@ -1,4 +1,12 @@
 package bmstu;
 
 public class Store {
+
+    private Map<String, ArrayList<Test>> store = new HashMap<>();
+    private static final String FAIL = "FAIL";
+    private static final Strinc SUCCESS = "SUCCESS";
+    private void putTest(Test test) {
+        String testId = test.getTest().getPackageId();
+        
+    }
 }
