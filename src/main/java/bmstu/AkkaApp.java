ce20acde1adf9c742518edc3d6e819ad3725f403
furstepnik <furstepnik@yandex.ru>
2021-12-24 14:07:30 +0300

    Scripted auto-commit on change (2021-12-24 14:07:30) by gitwatch.sh

--- src/main/java/bmstu/AkkaApp.java
+++ src/main/java/bmstu/AkkaApp.java
@@ -2,12 +2,18 @@ package bmstu;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.routing.Router;
+import akka.stream.ActorMaterializer;
 
 public class AkkaApp {
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("AkkaApp");
-        ActorRef 
+        ActorRef rout = system.actorOf(Props.create(Router.class, system));
+        final Http http = Http.get(system);
+        final ActorMaterializer 
     }
 }
