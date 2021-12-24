287acfa6af69d1bff37c77a66bb188a6bb3717b8
furstepnik <furstepnik@yandex.ru>
2021-12-24 14:47:48 +0300

    Scripted auto-commit on change (2021-12-24 14:47:48) by gitwatch.sh

--- src/main/java/bmstu/CreateRouter.java
+++ src/main/java/bmstu/CreateRouter.java
@@ -1,6 +1,24 @@
 package bmstu;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
+
 public class CreateRouter {
     private final ActorRef router;
-    
+    private final String NAME = "packageId";
+    private final int TIME_OUT = 3000;
+
+    public Route createRoute() {
+        return concat(
+                get(() ->
+                    parameter(NAME, id -> {
+                        Future<Object> res = Patterns.ask(router, id, TIME_OUT);
+                        return completeOKWithFuture(res, Jackson.marshaller());
+                })),
+                post(() ->
+                        entity)
+        )
+    }
+
 }
