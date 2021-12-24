15765f3db739bb2c36e8404dd4cb4e3097795e3e
furstepnik <furstepnik@yandex.ru>
2021-12-24 14:28:21 +0300

    Scripted auto-commit on change (2021-12-24 14:28:21) by gitwatch.sh

--- src/main/java/bmstu/Router.java
+++ src/main/java/bmstu/Router.java
@@ -1,4 +1,18 @@
 package bmstu;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.routing.RoundRobinPool;
+
 public class Router {
+    private final ActorRef actor;
+    private final ActorRef pool;
+
+    private final int WORK_NUMBER = 5;
+
+    public Router(ActorSystem system) {
+        actor = system.actorOf(Props.create(Store.class));
+        pool = system.actorOf(new RoundRobinPool(WORK_NUMBER).props(Props.create(TesterActor.class, storeActor)));
+    }
 }
